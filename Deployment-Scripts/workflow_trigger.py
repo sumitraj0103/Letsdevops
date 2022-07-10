288f97d8171ac4279a201d2519c66097a6c39af7
Sumit Raj <dip00283@scbcorp.onmicrosoft.com>
2022-07-10 23:58:05 +0800

    Added Python script

---
+++ Deployment-Scripts/workflow_trigger.py
@@ -0,0 +1,35 @@
+import openpyxl
+import os
+import requests
+import sys
+#pip install --upgrade pandas 
+#
+TOKEN= str(sys.argv[1])
+OWNER= str(sys.argv[2])
+REPO= str(sys.argv[3])
+Workflow_Name= str(sys.argv[4])
+pl_Baseline_Number= str(sys.argv[5])
+pl_Baseline_Revision = str(sys.argv[6])
+
+def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
+
+      headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {TOKEN}",
+      }
+
+      data = {
+        "event_type": Workflow_Name,
+        "client_payload": {
+          'baselinetag': pl_Baseline_Number,
+          'revision_number': pl_Baseline_Revision
+        }
+      }
+
+      requests.post(
+        f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",
+        json=data,
+        headers=headers
+      )
+
+trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision)
