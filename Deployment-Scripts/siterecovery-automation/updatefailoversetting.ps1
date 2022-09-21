9d58e40014807ee3e2f233f692f34044f09a712b
Raj <sumit.d.raj@capgemini.com>
2022-09-21 17:13:22 +0200

    Added script

---
+++ Deployment-Scripts/siterecovery-automation/updatefailoversetting.ps1
@@ -0,0 +1,27 @@
+﻿Login-AzAccount
+
+$recoveryvaultName="srstestfailovertest"
+$recovery_rg="east-us-rg"
+$replicaredVMName="windvm"
+$recoveryVMName="windvmtarget"
+$recovery_nsgID="UpdateMe"
+
+#Get the vault object by name and resource group and save it to the $vault PowerShell variable 
+$vault = Get-AzRecoveryServicesVault -Name $recoveryvaultName -ResourceGroupName $recovery_rg
+
+
+#set the vault context
+Set-ASRVaultContext -Vault $vault
+
+$ASRFabrics = Get-AzRecoveryServicesAsrFabric
+
+
+$ProtectionContainer = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $ASRFabrics[1]
+
+$ReplicatedVM1 = Get-AzRecoveryServicesAsrReplicationProtectedItem -FriendlyName $replicaredVMName -ProtectionContainer $ProtectionContainer
+
+#Set-AzRecoveryServicesAsrReplicationProtectedItem -InputObject $ReplicatedVM1 -Size "Standard_DS11" -UseManagedDisk True
+
+#Set-AzRecoveryServicesAsrReplicationProtectedItem -InputObject $ReplicatedVM1 -RecoveryNetworkSecurityGroupId ""
+
+Set-AzRecoveryServicesAsrReplicationProtectedItem -InputObject $ReplicatedVM1 -Name $recoveryVMName -RecoveryNetworkSecurityGroupId $recovery_nsgID
