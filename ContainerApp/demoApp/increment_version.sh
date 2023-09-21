38957861cb11a320b026be1d7b378bd6f5da6e17
Sumit <sumit.raj0103@gmail.com>
2023-09-21 20:48:51 +0200

    Update increment_version.sh

--- ContainerApp/demoApp/increment_version.sh
+++ ContainerApp/demoApp/increment_version.sh
@@ -15,4 +15,4 @@ NEW_VERSION=$(awk '{printf "%.1f", $0 + 0.1}' <<< "$CURRENT_VERSION")
 echo "$NEW_VERSION" > "$VERSION_FILE"
 
 # Print the new version
-echo "the new version value in Sumit$NEW_VERSION"
+echo "$NEW_VERSION"
